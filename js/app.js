64b5d0d22dfd961d579a9e6e383fa7cb4d5ccbaa
mostafizurRahaman <fahim654326@gmail.com>
2022-09-02 23:55:41 +0600

    categories section done

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// catagories load from api 
+const loadCategories = async () => {
+   try{
+         const url = `https://openapi.programming-hero.com/api/news/categories`;
+         const res = await fetch(url);
+         const data =await res.json(); 
+        showCategories(data.data.news_category); 
+   }catch(error){
+      console.log(error); 
+   }
+}
+
+const showCategories = (categories) => {
+      const categoriesContainer =  document.getElementById('category-container'); 
+      categories.forEach(category => {
+          const div = document.createElement('div'); 
+            div.innerHTML = `
+               <a href="#" class="text-decoration-none d-inline-block px-3 py-2 bg-primary text-dark fw-bold rounded text-white" > ${category.category_name}</a>
+            `
+            categoriesContainer.appendChild(div); 
+      });
+      console.log(categories); 
+}     
+
+loadCategories(); 
